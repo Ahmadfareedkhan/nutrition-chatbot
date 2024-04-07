78636b6302a1770ad9462f706311a3c11a2db9cc
Ahmadfareedkhan <ahmadkhanfareed388@gmail.com>
2024-04-07 16:58:47 +0500

    .

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
-import gradio as gr
-import openai
+import gradio as gr 
+from openai import OpenAI
+client = OpenAI()
 import os
 
 # Set your OpenAI API key here
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Hello!"}
+  ]
+)
+
+print(completion.choices[0].message)
+
 
 def chatbot_response(user_input):
     try:
         # Call to OpenAI's API
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
+        completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
                 {"role": "system", "content": "You are a nutrition consultant AI, capable of providing natural diet plans and emergency assistance based on user inputs."},
                 {"role": "user", "content": user_input},
             ]
         )
-        # Extracting the AI response
-        return response.choices[0].message.strip()
+
+        return completion.choices[0].message
+
     except Exception as e:
         return f"An error occurred: {str(e)}"
 
 # Creating the Gradio interface
 interface = gr.Interface(fn=chatbot_response,
-                         input=gr.inputs.Textbox(lines=5, placeholder="Type your question here..."),
-                         output="text",
+                         inputs=gr.Textbox(lines=5, placeholder="Type your question here..."),
+                         outputs="text",
                          title="HealthyBytes: Your AI Nutrition Consultant",
                          description="Ask me anything about nutrition, diet plans, or emergency assistance. For example, 'What should I eat to stay fit?' or 'What to do in case of a snake bite?'")
 
