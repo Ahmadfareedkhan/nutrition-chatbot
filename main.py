9b846c0e3c533df950bce0177e7b0e7250227306
Ahmadfareedkhan <ahmadkhanfareed388@gmail.com>
2024-04-07 19:19:02 +0500

    base model

--- main.py
+++ main.py
@@ -3,21 +3,6 @@ from openai import OpenAI
 client = OpenAI()
 import os
 
-# Set your OpenAI API key here
-# openai.api_key = os.getenv("OPENAI_API_KEY")
-
-
-# completion = client.chat.completions.create(
-#   model="gpt-3.5-turbo",
-#   messages=[
-#     {"role": "system", "content": "You are a helpful assistant."},
-#     {"role": "user", "content": "Hello!"}
-#   ]
-# )
-
-# print(completion.choices[0].message)
-
-
 def chatbot_response(user_input):
     try:
         # Call to OpenAI's API
@@ -42,4 +27,4 @@ interface = gr.Interface(fn=chatbot_response,
                          description="Ask me anything about nutrition, diet plans, or emergency assistance. For example, 'What should I eat to stay fit?' or 'What to do in case of a snake bite?'")
 
 if __name__ == "__main__":
-    interface.launch()
+    interface.launch(share=True)
